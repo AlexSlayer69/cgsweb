93a092b7ae4281ac2814aca9a3ed9e6665a4b43c
AlexSlayer69 <akulvjha2808@gmail.com>
2022-09-17 01:20:32 +0530

    header

---
+++ src/Components/Header/header.js
@@ -0,0 +1,13 @@
+import styles from './header_styles.css';
+
+function Navbar(){
+    return(
+        <ul>
+            <li>Home</li>
+            <li>Team</li>
+            <li>Contact</li>
+        </ul>
+    );
+}
+
+export default Navbar;
